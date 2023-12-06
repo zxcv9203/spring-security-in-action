e03efe17652c4e9b2a5a91927742c1c94c5fa1f1
yongckim <zxcv9203@naver.com>
2023-12-07 04:02:51 +0900

    :sparkles: UserDetailsService 구현체 변경

---
+++ chapter2/src/main/java/com/example/chapter2/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.chapter2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
+}

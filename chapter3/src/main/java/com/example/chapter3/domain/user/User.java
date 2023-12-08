872a313635c54e7ca9c5b209bffedeb4df092dbd
yongckim <zxcv9203@naver.com>
2023-12-08 12:14:03 +0900

    :sparkles: 엔티티와 UserDetails 구현체의 책임을 분리한 유저 도메인 생성

---
+++ chapter3/src/main/java/com/example/chapter3/domain/user/User.java
@@ -0,0 +1,43 @@
+package com.example.chapter3.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String authority;
+
+    protected User() {
+
+    }
+
+    public User(int id, String username, String password, String authority) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+}

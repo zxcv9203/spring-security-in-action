10c7e8162e81e69ce0f2eb94a549cbb1fa01ea29
yongckim <zxcv9203@naver.com>
2023-12-08 13:38:23 +0900

    :sparkles: JdbcUserDetailsManager 쿼리 커스텀 진행

--- chapter3/src/main/java/com/example/chapter3/config/UserManagementConfig.java
+++ chapter3/src/main/java/com/example/chapter3/config/UserManagementConfig.java
@@ -17,7 +17,14 @@ public class UserManagementConfig {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from authorities where username = ?";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+
+        return userDetailsManager;
     }
 
     @Bean

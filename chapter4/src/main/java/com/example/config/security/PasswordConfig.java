431d593efe60bc8219082695ac560a58cfb6c4d4
yongckim <zxcv9203@naver.com>
2023-12-09 14:52:40 +0900

    :sparkles: DelegatingPasswordEncoder를 이용하여 유연하게 PasswordEncoder를 설정하도록 구현

---
+++ chapter4/src/main/java/com/example/config/security/PasswordConfig.java
@@ -0,0 +1,27 @@
+package com.example.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder(16384, 8, 1, 32, 64));
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
+}

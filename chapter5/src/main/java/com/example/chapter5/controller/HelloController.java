213cbdceb21614f3b8cb9778b7d8b3a864424560
yongckim <zxcv9203@naver.com>
2023-12-10 12:30:20 +0900

    :sparkles: SecurityContextHolder에서 SecurityContext 가져오도록 구현

--- chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
+++ chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.chapter5.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,9 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String getHello() {
-        return "hello";
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication user = context.getAuthentication();
+
+        return "hello, " + user.getName() + "!";
     }
 }

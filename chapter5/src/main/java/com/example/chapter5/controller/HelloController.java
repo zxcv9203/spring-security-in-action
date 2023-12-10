4bf3190ba459c528033ffeb96d11bf3088901a30
yongckim <zxcv9203@naver.com>
2023-12-10 12:06:32 +0900

    :sparkles: 테스트용 엔드포인트 추가

---
+++ chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.chapter5.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hello";
+    }
+}

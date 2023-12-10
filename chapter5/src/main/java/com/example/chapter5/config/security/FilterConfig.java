0902dc146b0a7b169221b91a43149771dca77d21
yongckim <zxcv9203@naver.com>
2023-12-10 12:06:21 +0900

    :sparkles: AuthenticationProvider 필터 등록

---
+++ chapter5/src/main/java/com/example/chapter5/config/security/FilterConfig.java
@@ -0,0 +1,25 @@
+package com.example.chapter5.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class FilterConfig {
+    private final AuthenticationProvider authenticationProvider;
+
+    public FilterConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
+        return http.build();
+    }
+}

41113783ad48811069fb8c18891fd3acb9f635d6
yongckim <zxcv9203@naver.com>
2023-12-11 17:28:07 +0900

    :sparkles: 시큐리티 인증에 사용되는 LoginUser 객체 정의

---
+++ chapter6/src/main/java/com/example/chapter6/model/LoginUser.java
@@ -0,0 +1,53 @@
+package com.example.chapter6.model;
+
+import com.example.chapter6.domain.user.model.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+public class LoginUser implements UserDetails {
+
+    private final User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities().stream()
+                .map(a -> new SimpleGrantedAuthority(a.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

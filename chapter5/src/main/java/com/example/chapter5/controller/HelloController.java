72edf97cc37edce1a2b3c65c7a5b666247bdadc8
yongckim <zxcv9203@naver.com>
2023-12-11 10:36:49 +0900

    :sparkles: 생성한 스레드에 보안 컨텍스트가 전달되지 않는 경우 DelegatingSecurityContextCallable을 이용하여 값 전달하는 예제 작성

--- chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
+++ chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.chapter5.controller;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -22,5 +27,22 @@ public class HelloController {
         String username = context.getAuthentication().getName();
 
         System.out.println("good bye, " + username);
+    }
+
+    @GetMapping("/hi")
+    public String hi() throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        try {
+            DelegatingSecurityContextCallable<String> contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "hi, " + executorService.submit(contextTask).get() + "!";
+        } finally {
+            executorService.shutdown();
+        }
     }
 }

ca4f43227cbc0a763eb870563f0ad916b9ca73e5
yongckim <zxcv9203@naver.com>
2023-12-11 15:55:22 +0900

    :sparkles: 핸들러를 통해 성공, 실패에 대한 세부 동작 정의

--- chapter5/src/main/java/com/example/chapter5/config/security/FilterConfig.java
+++ chapter5/src/main/java/com/example/chapter5/config/security/FilterConfig.java
@@ -6,19 +6,30 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class FilterConfig {
     private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
 
-    public FilterConfig(AuthenticationProvider authenticationProvider) {
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
+    public FilterConfig(AuthenticationProvider authenticationProvider, AuthenticationSuccessHandler authenticationSuccessHandler, AuthenticationFailureHandler authenticationFailureHandler) {
         this.authenticationProvider = authenticationProvider;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
-                .formLogin(f -> f.defaultSuccessUrl("/main", true));
+                .formLogin(f -> {
+                    f.defaultSuccessUrl("/main", true);
+                    f.successHandler(authenticationSuccessHandler);
+                    f.failureHandler(authenticationFailureHandler);
+                });
         http.authenticationProvider(authenticationProvider);
         return http.build();
     }

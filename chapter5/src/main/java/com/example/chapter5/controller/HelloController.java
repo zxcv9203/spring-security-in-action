970328c0fd530d6c3e6c12499e753fb1e701b474
yongckim <zxcv9203@naver.com>
2023-12-11 09:50:42 +0900

    :sparkles: 비동기 설정시 다른 스레드로 실행되어 SecurityContext 정보를 불러오지 못하는 시나리오 작성

--- chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
+++ chapter5/src/main/java/com/example/chapter5/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.chapter5.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String getHello(Authentication user) {
         return "hello, " + user.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public void goodbye() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+
+        System.out.println("good bye, " + username);
     }
 }

ed0f76967bba6e4338b4f9ac2faae0301ab2830b
yongckim <zxcv9203@naver.com>
2023-12-11 18:03:18 +0900

    :sparkles: 상품 조회 기능 구현

---
+++ chapter6/src/main/java/com/example/chapter6/domain/product/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.chapter6.domain.product;
+
+import com.example.chapter6.domain.product.model.Product;
+import com.example.chapter6.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+}

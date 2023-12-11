0f7a4679abf843037c5ea3a12db511e1d9512e5a
yongckim <zxcv9203@naver.com>
2023-12-11 17:07:04 +0900

    :sparkles: 패스워드 인코더 설정 추가

---
+++ chapter6/src/main/java/com/example/chapter6/config/security/PasswordConfig.java
@@ -0,0 +1,20 @@
+package com.example.chapter6.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder(16384, 8, 1, 32, 64);
+    }
+}

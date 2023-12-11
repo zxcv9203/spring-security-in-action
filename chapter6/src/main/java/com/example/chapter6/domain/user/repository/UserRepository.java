3cf7bb792da422712695a3912fb9cd0199410d7a
yongckim <zxcv9203@naver.com>
2023-12-11 17:24:07 +0900

    :sparkles: 유저에 대한 Repository 추가

---
+++ chapter6/src/main/java/com/example/chapter6/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.chapter6.domain.user.repository;
+
+import com.example.chapter6.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
+}

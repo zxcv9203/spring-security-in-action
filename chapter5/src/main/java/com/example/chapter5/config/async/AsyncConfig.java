1165ccef63b0379a80db984368e6b88c77a21b34
yongckim <zxcv9203@naver.com>
2023-12-11 10:09:06 +0900

    :sparkles: 별도의 스레드에서 실행시 보안 컨텍스트를 생성된 스레드에 전달되는 전략을 사용하도록 변경

--- chapter5/src/main/java/com/example/chapter5/config/async/AsyncConfig.java
+++ chapter5/src/main/java/com/example/chapter5/config/async/AsyncConfig.java
@@ -1,10 +1,16 @@
 package com.example.chapter5.config.async;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
-
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
 }

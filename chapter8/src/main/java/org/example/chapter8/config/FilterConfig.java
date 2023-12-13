d2dd77ed91fef0c57833eb0aa0b66e1a3f83111b
yongckim <zxcv9203@naver.com>
2023-12-13 16:03:02 +0900

    :sparkles: 선택기 메서드를 통해 특정 경로에 따라 접근 권한 제어

---
+++ chapter8/src/main/java/org/example/chapter8/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package org.example.chapter8.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/hello").hasRole("ADMIN")
+                        .requestMatchers("/hi").hasRole("DEVELOPER"))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}

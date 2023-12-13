40cb4a21ad2f1c3adf134486667e278bcc902e98
yongckim <zxcv9203@naver.com>
2023-12-13 20:39:39 +0900

    :sparkles: csrf disable 처리

--- chapter8/src/main/java/org/example/chapter8/config/FilterConfig.java
+++ chapter8/src/main/java/org/example/chapter8/config/FilterConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +12,8 @@ public class FilterConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests(authz -> authz
                         .requestMatchers("/hello").hasRole("ADMIN")
                         .requestMatchers("/hi").hasRole("DEVELOPER")

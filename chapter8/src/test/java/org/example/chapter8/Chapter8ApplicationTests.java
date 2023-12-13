72027db40aeee0ce44d637b4105394ec0d236066
yongckim <zxcv9203@naver.com>
2023-12-13 15:25:35 +0900

    :tada: 챕터 8 프로젝트 초기화

---
+++ chapter8/src/test/java/org/example/chapter8/Chapter8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chapter8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

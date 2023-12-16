a4850f62499eb9acad2ed1b20ed32f8b28551c5d
yongckim <zxcv9203@naver.com>
2023-12-16 14:53:16 +0900

    :sparkles: OncePerRequestFilter를 확장하여 리팩토링

--- chapter9/src/main/java/org/example/chapter9/config/security/filter/AuthenticationLogginFilter.java
+++ chapter9/src/main/java/org/example/chapter9/config/security/filter/AuthenticationLogginFilter.java
@@ -3,22 +3,24 @@ package org.example.chapter9.config.security.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class AuthenticationLogginFilter implements Filter {
+public class AuthenticationLogginFilter extends OncePerRequestFilter {
     private final Logger logger = LoggerFactory.getLogger(AuthenticationLogginFilter.class);
 
+    // OncePerRequestFilter는 HTTP 필터만 지원합니다.
+    // 이 때문에 HttpServletRequest 및 HttpServletResponse로 매개 변수를 직접 지정했습니다.
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestId = request.getHeader("Request-Id");
 
-        String requestId = httpRequest.getHeader("Request-Id"); // 요청헤더에서 ID를 얻음
+        logger.info("Successfully authentication request with id " + requestId);
 
-        logger.info("Successfully authenticated request with id " + requestId); // 요청 ID의 값과 이벤트 기록
-
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 }

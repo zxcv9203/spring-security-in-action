47b5af762ad5302104e5f506841324ae9b711a07
yongckim <zxcv9203@naver.com>
2023-12-17 22:55:12 +0900

    :sparkles: 기본 MainPage 구현

---
+++ chapter10/src/main/java/org/example/chapter10/controller/MainController.java
@@ -0,0 +1,13 @@
+package org.example.chapter10.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main() {
+        return "main.html";
+    }
+}

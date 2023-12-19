4d95642c5cb9d0a3011b190befa621e0e764bbf0
yongckim <zxcv9203@naver.com>
2023-12-19 20:39:12 +0900

    :sparkles: CSRF Token 값을 오인식하는 버그 해결
    - 스프링 시큐리티 6.0부터 전달받은 토큰 값을 Base64로 디코딩
    - CSRF Token 핸들러를 Base64 디코딩하지 않는 구현체로 변경

--- chapter10/src/main/java/org/example/chapter10/repository/CustomCsrfTokenRepository.java
+++ chapter10/src/main/java/org/example/chapter10/repository/CustomCsrfTokenRepository.java
@@ -15,8 +15,6 @@ import java.util.UUID;
 
 @Repository
 public class CustomCsrfTokenRepository implements CsrfTokenRepository {
-
-    private final Logger logger = LoggerFactory.getLogger(CustomCsrfTokenRepository.class);
     private final JpaTokenRepository jpaTokenRepository;
 
     public CustomCsrfTokenRepository(JpaTokenRepository jpaTokenRepository) {
@@ -26,7 +24,6 @@ public class CustomCsrfTokenRepository implements CsrfTokenRepository {
     // 애플리케이션이 새 토큰을 생성할때 호출하는 메서드
     @Override
     public CsrfToken generateToken(HttpServletRequest request) {
-        logger.error("토큰 생성");
         String uuid = UUID.randomUUID().toString();
         return new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", uuid);
     }
@@ -37,34 +34,29 @@ public class CustomCsrfTokenRepository implements CsrfTokenRepository {
     // 데이터베이스에 해당 ID가 존재하면 새로운 토큰으로 업데이트하고 없다면 데이터베이스에 칼럼을 추가합니다.
     @Override
     public void saveToken(CsrfToken token, HttpServletRequest request, HttpServletResponse response) {
-        logger.error("토큰 저장");
         String identifier = request.getHeader("X-IDENTIFIER");
         // 클라이언트 ID로 데이터베이스에서 토큰을 얻음
         Optional<Token> existingToken = jpaTokenRepository.findTokenByIdentifier(identifier);
 
         // ID가 존재하면 새로 생성된 값으로 토큰의 값을 업데이트
         if (existingToken.isPresent()) {
-            logger.warn("토큰 존재");
             Token csrfToken = existingToken.get();
-            csrfToken.changeToken(csrfToken.getToken());
+            csrfToken.changeToken(token.getToken());
         } else {
             // ID가 존재하지 않으면 생성된 CSRF 값과 ID로 새 레코드 생성
             Token csrfToken = new Token(identifier, token.getToken());
             jpaTokenRepository.save(csrfToken);
-            logger.warn("토큰 저장 완료 : {}", csrfToken);
         }
     }
 
     // 토큰 세부 정보가 있으면 로드하고 없으면 null 반환
     @Override
     public CsrfToken loadToken(HttpServletRequest request) {
-        logger.error("토큰 로드");
-        String identifer = request.getHeader("X-IDENTIFIER");
+        String identifier = request.getHeader("X-IDENTIFIER");
 
-        Optional<Token> existingToken = jpaTokenRepository.findTokenByIdentifier(identifer);
+        Optional<Token> existingToken = jpaTokenRepository.findTokenByIdentifier(identifier);
 
         if (existingToken.isPresent()) {
-            logger.warn("토큰 값 : {}", existingToken.get().getToken());
             Token csrfToken = existingToken.get();
             return new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", csrfToken.getToken());
         }

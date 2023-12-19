3523cd61d0ab956780a57f3cc96a28a50c3ebde1
yongckim <zxcv9203@naver.com>
2023-12-19 21:54:09 +0900

    :sparkles: CorsConfigurer를 이용하여 CORS 적용

--- chapter10/src/main/java/org/example/chapter10/config/filter/FilterConfig.java
+++ chapter10/src/main/java/org/example/chapter10/config/filter/FilterConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 public class FilterConfig {
@@ -12,6 +15,12 @@ public class FilterConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(c -> c.configurationSource(request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedOrigins(List.of("example.com", "example.org"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+            return config;
+        }));
         http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
 
         return http.build();

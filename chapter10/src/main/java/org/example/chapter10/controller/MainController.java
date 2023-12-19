c41c7c8fb90f0c0a5b7dc324746f3e7d83cbe15f
yongckim <zxcv9203@naver.com>
2023-12-19 21:13:44 +0900

    :sparkles: CORS 테스트를 위한 엔드포인트 추가

--- chapter10/src/main/java/org/example/chapter10/controller/MainController.java
+++ chapter10/src/main/java/org/example/chapter10/controller/MainController.java
@@ -1,13 +1,29 @@
 package org.example.chapter10.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/main")
+    private final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    // /test 엔드포인트를 요청하는 main.html 페이지 정의
+    @GetMapping("/")
     public String main() {
         return "main.html";
     }
+
+    // CORS 작동을 확인하기 위해 다른 출처에서 호출할 엔드포인트 정의
+    @PostMapping("/test")
+    @ResponseBody
+    public String test() {
+        log.info("Test Method called");
+        return "HELLO";
+    }
+
 }

3c6bfc3c250c1e5ad064a07a0edb49d1c5501872
yongckim <zxcv9203@naver.com>
2023-12-19 21:43:11 +0900

    :sparkles: @CrossOrigin 어노테이션을 활용하여 CORS 허용

--- chapter10/src/main/java/org/example/chapter10/controller/MainController.java
+++ chapter10/src/main/java/org/example/chapter10/controller/MainController.java
@@ -3,6 +3,7 @@ package org.example.chapter10.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,7 @@ public class MainController {
 
     // CORS 작동을 확인하기 위해 다른 출처에서 호출할 엔드포인트 정의
     @PostMapping("/test")
+    @CrossOrigin("http://localhost:8080")
     @ResponseBody
     public String test() {
         log.info("Test Method called");

9b57a6d0dd427ca9f8cf9a0a8cc83c4a0fb5c357
yongckim <zxcv9203@naver.com>
2023-12-19 22:10:42 +0900

    :tada: 챕터11 프로젝트 초기화

---
+++ chapter11/src/test/java/org/example/chapter11/Chapter11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chapter11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

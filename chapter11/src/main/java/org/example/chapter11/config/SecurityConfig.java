4de7f8a743a81e523e9bb3cb99830bbd2b133087
yongckim <zxcv9203@naver.com>
2023-12-20 21:35:20 +0900

    :bug: iframe 헤더 허용을 통해 H2 화면이 보이도록 설정

--- chapter11/src/main/java/org/example/chapter11/config/SecurityConfig.java
+++ chapter11/src/main/java/org/example/chapter11/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package org.example.chapter11.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -16,6 +19,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable);
+
+        // iframe을 사용하도록 허용하여 H2 화면이 구성될 수 있도록 설정
+        // https://dukcode.github.io/spring/h2-console-with-spring-security/
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         // TODO 대칭키, 비대칭키를 이용한 인증 서버 <-> 비즈니스 논리 서버 간의 요청 검증하기
 

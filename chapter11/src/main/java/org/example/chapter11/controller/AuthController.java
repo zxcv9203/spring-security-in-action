a5b784dd5fc863c50238a25fabc4d5da948848b1
yongckim <zxcv9203@naver.com>
2023-12-20 21:03:24 +0900

    :sparkles: 인증 엔드포인트 추가

---
+++ chapter11/src/main/java/org/example/chapter11/controller/AuthController.java
@@ -0,0 +1,38 @@
+package org.example.chapter11.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.chapter11.model.Otp;
+import org.example.chapter11.model.User;
+import org.example.chapter11.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @PostMapping("/user/auth")
+    public void auth(@RequestBody User user) {
+        userService.auth(user);
+    }
+
+    @PostMapping("/otp/check")
+    public void check(@RequestBody Otp otp, HttpServletResponse response) {
+        if (userService.check(otp)) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}

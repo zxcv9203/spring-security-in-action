fbf9439d65c33f4f93a5f5cc6203801602854b66
yongckim <zxcv9203@naver.com>
2023-12-24 23:50:17 +0900

    :tada: 챕터 13 프로젝트 초기화

---
+++ chapter13/src/test/java/org/example/chapter13/Chapter13ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chapter13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

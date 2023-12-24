2ba8d868b01be371dc6a9c559817d34fe7de31d8
yongckim <zxcv9203@naver.com>
2023-12-24 23:25:03 +0900

    :sparkles: OAuth2 앱 테스트

--- chapter12/src/main/java/org/example/chapter12/controller/MainController.java
+++ chapter12/src/main/java/org/example/chapter12/controller/MainController.java
@@ -1,13 +1,20 @@
 package org.example.chapter12.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    private final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @GetMapping
-    public String main() {
+    public String main(OAuth2AuthenticationToken token) {
+        log.info(String.valueOf(token.getPrincipal()));
+
         return "main.html";
     }
 }

3b3b080d23907e604a07e122df3ae00d9fc0c935
yongckim <zxcv9203@naver.com>
2023-12-24 23:06:26 +0900

    :sparkles: 구성 파일로 OAuth2 공급자 등록

--- chapter12/src/main/java/org/example/chapter12/config/security/FilterConfig.java
+++ chapter12/src/main/java/org/example/chapter12/config/security/FilterConfig.java
@@ -4,28 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class FilterConfig {
-
-    @Bean
-    public ClientRegistrationRepository clientRepository() {
-        return new InMemoryClientRegistrationRepository(clientRegistration());
-    }
-
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB
-                .getBuilder("github")
-                .clientId("7c265394b291b3a3738d")
-                .clientSecret("28863009357d77b1f8c85088892980bdaccf26a5")
-                .build();
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.oauth2Login(Customizer.withDefaults());

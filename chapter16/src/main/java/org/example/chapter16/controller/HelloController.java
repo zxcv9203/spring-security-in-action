5b9db97ac007e51f71f8e2928b8754d03768b15b
yongckim <zxcv9203@naver.com>
2023-12-25 23:13:21 +0900

    :sparkles: 테스트용 엔드포인트 추가

--- chapter16/src/main/java/org/example/chapter16/controller/HelloController.java
+++ chapter16/src/main/java/org/example/chapter16/controller/HelloController.java
@@ -2,7 +2,10 @@ package org.example.chapter16.controller;
 
 import org.example.chapter16.service.NameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -16,5 +19,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + nameService.getName();
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
 }

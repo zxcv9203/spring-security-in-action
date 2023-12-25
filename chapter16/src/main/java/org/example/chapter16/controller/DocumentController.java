81fb7efc96a7e5ba09736a1c17c7a09d1fa0b666
yongckim <zxcv9203@naver.com>
2023-12-26 02:38:45 +0900

    :sparkles: 테스트용 엔드포인트 작성

---
+++ chapter16/src/main/java/org/example/chapter16/controller/DocumentController.java
@@ -0,0 +1,23 @@
+package org.example.chapter16.controller;
+
+import org.example.chapter16.model.Document;
+import org.example.chapter16.service.DocumentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentController {
+
+    private final DocumentService documentService;
+
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @GetMapping("/documents/{code}")
+    public Document getDetails(@PathVariable String code) {
+        return documentService.getDocument(code);
+    }
+
+}

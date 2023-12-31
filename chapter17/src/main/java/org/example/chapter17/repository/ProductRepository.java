130bb536582e44aa4ab7f8a583ec4304f7e3ea15
yongckim <zxcv9203@naver.com>
2023-12-31 20:00:37 +0900

    :sparkles: JPA 레포지터리 정의

--- chapter17/src/main/java/org/example/chapter17/repository/ProductRepository.java
+++ chapter17/src/main/java/org/example/chapter17/repository/ProductRepository.java
@@ -1,12 +1,13 @@
 package org.example.chapter17.repository;
 
 import org.example.chapter17.domain.Product;
+import org.example.chapter17.domain.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PostFilter;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     @PostFilter("filterObject.owner == authentication.name")
     List<Product> findProductByNameContains(String text);

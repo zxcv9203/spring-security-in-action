70c8f8b26bf59483c7e6a49674c6df48374960e4
yongckim <zxcv9203@naver.com>
2023-12-31 20:30:46 +0900

    :sparkles: 쿼리에 SpEL을 이용하여 필터링

--- chapter17/src/main/java/org/example/chapter17/repository/ProductRepository.java
+++ chapter17/src/main/java/org/example/chapter17/repository/ProductRepository.java
@@ -1,14 +1,20 @@
 package org.example.chapter17.repository;
 
-import org.example.chapter17.domain.Product;
 import org.example.chapter17.domain.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
-    @PostFilter("filterObject.owner == authentication.name")
-    List<Product> findProductByNameContains(String text);
+    @Query(
+            """
+                    SELECT p FROM ProductEntity p
+                    WHERE p.name LIKE %:text%
+                    AND p.owner=?#{authentication.name}                  
+                            """
+    )
+    List<ProductEntity> findProductByNameContains(String text);
+
 }

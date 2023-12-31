32d16d55a06c22fa6114a29989e717f231c86334
yongckim <zxcv9203@naver.com>
2023-12-31 19:55:47 +0900

    :sparkles: 도메인 entity 정의

---
+++ chapter17/src/main/java/org/example/chapter17/domain/ProductEntity.java
@@ -0,0 +1,38 @@
+package org.example.chapter17.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String owner;
+
+    protected ProductEntity() {
+
+    }
+
+    public ProductEntity(Long id, String name, String owner) {
+        this.id = id;
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}

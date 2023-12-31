3b5a6941720168aa832e8ac13caef7952599baeb
yongckim <zxcv9203@naver.com>
2023-12-31 18:32:33 +0900

    :sparkles: @PreFilter를 사용하여 사전 필터링 적용

---
+++ chapter17/src/main/java/org/example/chapter17/service/ProductService.java
@@ -0,0 +1,15 @@
+package org.example.chapter17.service;
+
+import org.example.chapter17.domain.Product;
+import org.springframework.security.access.prepost.PreFilter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @PreFilter("filterObject.owner == authentication.name")
+    public List<Product> sellProducts(List<Product> products) {
+        return products;
+    }
+}

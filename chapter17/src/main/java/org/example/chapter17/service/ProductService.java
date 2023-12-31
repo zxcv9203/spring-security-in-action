2714336492e3fe725dbdc2a631013ecec1483a4d
yongckim <zxcv9203@naver.com>
2023-12-31 18:52:09 +0900

    :sparkles: 사후 필터링을 사용하여 인증된 사용자의 상품만 조회하도록 처리

--- chapter17/src/main/java/org/example/chapter17/service/ProductService.java
+++ chapter17/src/main/java/org/example/chapter17/service/ProductService.java
@@ -1,15 +1,28 @@
 package org.example.chapter17.service;
 
 import org.example.chapter17.domain.Product;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
     @PreFilter("filterObject.owner == authentication.name")
     public List<Product> sellProducts(List<Product> products) {
+        return products;
+    }
+
+    @PostFilter("filterObject.owner == authentication.name")
+    public List<Product> findProducts() {
+        List<Product> products = new ArrayList<>();
+
+        products.add(new Product("beer", "kim"));
+        products.add(new Product("candy", "kim"));
+        products.add(new Product("chocolate", "park"));
+
         return products;
     }
 }

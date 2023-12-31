5de2dfd6a216b22d52582d511430b5e4b10e1980
yongckim <zxcv9203@naver.com>
2023-12-31 18:42:21 +0900

    :sparkles: 사전 필터링의 경우 불변 객체를 사용하는 경우 수정이 불가능해 예외가 발생하는 케이스 작성

--- chapter17/src/main/java/org/example/chapter17/controller/ProductController.java
+++ chapter17/src/main/java/org/example/chapter17/controller/ProductController.java
@@ -5,7 +5,6 @@ import org.example.chapter17.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,12 +18,11 @@ public class ProductController {
 
     @GetMapping("/sell")
     public List<Product> sellProducts() {
-        List<Product> products = new ArrayList<>();
-
-        products.add(new Product("beer", "kim"));
-        products.add(new Product("candy", "kim"));
-        products.add(new Product("chocolate", "park"));
-
+        List<Product> products = List.of(
+                new Product("beer", "kim"),
+                new Product("candy", "kim"),
+                new Product("chocolate", "park")
+        );
         return productService.sellProducts(products);
     }
 }

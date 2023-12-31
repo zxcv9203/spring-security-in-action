afe975d985f149cc435df2af59c8e9f7002a1b98
yongckim <zxcv9203@naver.com>
2023-12-31 18:22:13 +0900

    :sparkles: 사용자 인증에 사용되는 기본 설정 추가

---
+++ chapter17/src/main/java/org/example/chapter17/config/UserManagementConfig.java
@@ -0,0 +1,41 @@
+package org.example.chapter17.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableMethodSecurity
+public class UserManagementConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails readUser = User.withUsername("kim")
+                .password("1234")
+                .authorities("read")
+                .build();
+
+        UserDetails writeUser = User.withUsername("park")
+                .password("1234")
+                .authorities("write")
+                .build();
+
+        userDetailsService.createUser(readUser);
+        userDetailsService.createUser(writeUser);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}

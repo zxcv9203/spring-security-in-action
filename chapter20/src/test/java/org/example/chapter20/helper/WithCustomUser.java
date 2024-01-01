d84a224500a4355192cf8ec8ec095b5c166f7b80
yongckim <zxcv9203@naver.com>
2024-01-02 01:33:44 +0900

    :sparkles: 맞춤형 어노테이션을 팩터리 클래스에 연결

--- chapter20/src/test/java/org/example/chapter20/helper/WithCustomUser.java
+++ chapter20/src/test/java/org/example/chapter20/helper/WithCustomUser.java
@@ -1,9 +1,12 @@
 package org.example.chapter20.helper;
 
+import org.springframework.security.test.context.support.WithSecurityContext;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = CustomSecurityContextFactory.class)
 public @interface WithCustomUser {
 
     String username();

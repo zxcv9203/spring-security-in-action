1cc7179c7648dc79d5b147b3b2b0ff03b3984ddd
yongckim <zxcv9203@naver.com>
2024-01-02 00:14:11 +0900

    :sparkles: 인증된 사용자 인 경우 200 반환

--- chapter20/src/test/java/org/example/chapter20/controller/MainControllerTest.java
+++ chapter20/src/test/java/org/example/chapter20/controller/MainControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -19,7 +21,15 @@ class MainControllerTest {
 
     @Test
     public void unauthenticatedTest() throws Exception {
-        mvc.perform(get("/hello"))
+        mvc.perform(get("/"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "mary")
+    public void authenticatedTest() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
     }
 }

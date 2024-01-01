276faadca372bbfe7d1e9520fde9101bc4852acf
yongckim <zxcv9203@naver.com>
2024-01-01 21:58:37 +0900

    :sparkles: 인증되지 않은 사용자가 요청시 401 반환 테스트

---
+++ chapter20/src/test/java/org/example/chapter20/controller/MainControllerTest.java
@@ -0,0 +1,25 @@
+package org.example.chapter20.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void unauthenticatedTest() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isUnauthorized());
+    }
+}

3d38703c060a2134bd513ba29be31d806f10ebe0
yongckim <zxcv9203@naver.com>
2024-01-02 01:01:54 +0900

    :sparkles: UserDeatilsService에서 사용자 불러와서 인증된 요청 테스트

--- chapter20/src/test/java/org/example/chapter20/controller/MainControllerTest.java
+++ chapter20/src/test/java/org/example/chapter20/controller/MainControllerTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -30,6 +29,13 @@ class MainControllerTest {
     public void authenticatedTest() throws Exception {
         mvc.perform(get("/").with(user("mary")))
                 .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails("kim")
+    public void userDetailsServiceTest() throws Exception {
+        mvc.perform(get("/"))
                 .andExpect(status().isOk());
     }
 }
